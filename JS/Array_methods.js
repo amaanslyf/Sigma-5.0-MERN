02fc7f3e50e32dfc23486607176d77792b66b5fd
amaanslyf <amanshaikh477@gmail.com>
2025-03-20 10:18:30 +0530

    Added new  files

---
+++ JS/Array_methods.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+arr[20] = 20;
+console.log(arr.length); // 21 the middle blocks or indices of arraty are emplty but they are still counted as length of array
+//array methods
+let arr1=["audi",'bmw','benz','volvo','jaguar'];
+console.log(arr1);
+arr1.push('toyota'); //push method is used to add an element at the end of the array
+console.log(arr1);
+arr1.pop(); //pop method is used to remove the last element from the array and returns it
+console.log(arr1);
+arr1.shift(); //shift method is used to remove the first element from the array and returns it
+console.log(arr1);
+arr1.unshift('bmw'); //unshift method is used to add an element at the beginning of the array
+console.log(arr1);      
+arr.length; //length property is used to get the length of the array
+
+
