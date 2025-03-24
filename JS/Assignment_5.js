90a13081b65eac0b70cefe3e4153bc3f97c2d62b
amaanslyf <amanshaikh477@gmail.com>
2025-03-24 12:35:22 +0530

    Added new  files

---
+++ JS/Assignment_5.js
@@ -0,0 +1,21 @@
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+
+const car = {
+    name: "Maruti Suzuki",
+    model: "Maruti Suzuki Dzire",
+    color: "white",
+};
+console.log(car.name);
+
+
+const Person = { 
+    name: "Jane Doe",
+    age: 21,
+    city: "New Jersey", 
+}; 
+
+Person.city = "New York"; 
+Person.country = "United States"; 
+console.log(Person);
