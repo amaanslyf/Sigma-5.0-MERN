add33146291c44c7b2ee6c90a79d068bab76e901
amaanslyf <amanshaikh477@gmail.com>
2025-04-07 21:11:09 +0530

    Added new  files

---
+++ JS/Breakpoints_callStack_learning_example.js
@@ -0,0 +1,12 @@
+function one(){
+    return 1;
+
+}
+function two(){
+    return one()+one();
+}
+function three(){
+    return two()+one();
+}
+three();
+// The code above defines three functions: one, two, and three.
