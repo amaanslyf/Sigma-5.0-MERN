b1abe062c60bfd483b48810a533e5de247a96f9d
amaanslyf <amanshaikh477@gmail.com>
2025-04-08 20:59:16 +0530

    Added new  files

--- JS/Callback_hell_example.js
+++ JS/Callback_hell_example.js
@@ -1,18 +1,55 @@
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange) {
-  setTimeout(() => {
-    h1.style.color = color;
-    if (nextColorChange) nextColorChange();
-  }, delay);
+// function changeColor(color, delay, nextColorChange) {
+//   setTimeout(() => {
+//     h1.style.color = color;
+//     if (nextColorChange) nextColorChange();
+//   }, delay);
+// }
+
+// changeColor("red", 1000, () => {
+//   changeColor("orange", 1000, () => {
+//     changeColor("green", 1000, () => {
+//       changeColor("yellow", 1000, () => {
+//         changeColor("blue", 1000);
+//       });
+//     });
+//   });
+// });
+// The above code is an example of callback hell, where multiple nested callbacks make the code hard to read and maintain.
+
+//SOLVED USING PROMISES
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      h1.style.color = color;
+      resolve("Color changed to " + color);
+    }, delay);
+  });
 }
 
-changeColor("red", 1000, () => {
-  changeColor("orange", 1000, () => {
-    changeColor("green", 1000, () => {
-      changeColor("yellow", 1000, () => {
-        changeColor("blue", 1000);
-      });
-    });
-  });
-});
+changeColor("red", 1000)
+  .then((result) => {
+    console.log(result);
+    return changeColor("orange", 1000);
+  })
+  .then((result) => {
+    console.log(result);
+    return changeColor("green", 1000);
+  })
+  .then((result) => {
+    console.log(result);
+    return changeColor("yellow", 1000);
+  })
+  .then((result) => {
+    console.log(result);
+    return changeColor("blue", 1000);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+
+
+
