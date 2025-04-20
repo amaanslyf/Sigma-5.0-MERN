c8645347374149cc7834efa7817882ade915bab1
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-04-20 16:58:22 +0530

    added new file

---
+++ Node.js/Export_in_files.js
@@ -0,0 +1,9 @@
+module.exports.sum = (a,b)=>{
+    return a+b;
+}
+module.exports.sub = (a,b)=>{
+    return a-b;
+}
+module.exports.g =9.8;
+
+module.exports.pi = 3.14;
---
+++ Node.js/Export_in_files2.js
@@ -0,0 +1,6 @@
+const exported = require('./Export_in_files.js');
+console.log(exported);
+console.log(exported.sum(5, 6)); // 11
+console.log(exported.sub(5, 6)); // -1
+console.log(exported.g); // 9.8
+console.log(exported.pi); // 3.14
