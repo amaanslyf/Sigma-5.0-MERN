2403f8e514c3f919a02e8861885fb43cd52188af
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-04-22 20:29:27 +0530

    added new files

--- Ejs/index.js
+++ Ejs/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');  //package for handling file paths
 
-const port=8080;
+const port = 8080;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Set the views directory so that EJS can find the templates from its parent directory too
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is  running on port ${port}`);
 });
