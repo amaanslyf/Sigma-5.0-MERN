cef15444b7ee99acc1131f0ed390e57728c4db8b
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-10 13:22:14 +0530

    added new files

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -4,14 +4,17 @@ const mongoose=require('mongoose');
 const Listing=require('./models/listing');
 const path=require('path');
 const methodOverride=require('method-override');
+const ejsMate=require('ejs-mate');
 
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.engine('ejs',ejsMate);
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'/public')));
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,7 +36,7 @@ app.listen(8080,()=>{
 });
 
 app.get('/',(req,res)=>{
-    console.log("Hello i m root");
+    res.send("Hello i m root");
 });
 
 
