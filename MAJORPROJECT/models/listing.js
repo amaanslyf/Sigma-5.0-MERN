e2e5fbb9431840c8a0fa257d27736b4551bf10eb
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-26 12:19:00 +0530

    added new files

--- MAJORPROJECT/models/listing.js
+++ MAJORPROJECT/models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -32,5 +33,13 @@ const listingSchema = new Schema({
   ],
 });
 
+//mongoose post middleware to delete reviews when a listing is deleted
+listingSchema.post("findOneAndDelete", async(listing) => {
+  if(listing){
+  await Review.deleteMany({_id :{ $in: listing.reviews }});
+}
+});
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
+
