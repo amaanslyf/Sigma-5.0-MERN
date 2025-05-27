539e7d8c5a68edf9d647d9c41be3dcb6ea182021
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-27 13:35:17 +0530

    added new files

--- MAJORPROJECT/routes/listing.js
+++ MAJORPROJECT/routes/listing.js
@@ -36,6 +36,10 @@ router.get('/new', (req, res) => {
 router.get('/:id', wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate('reviews');
+    if (!listing) {
+        req.flash('error', 'Listing not found');
+        return res.redirect('/listings');
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -62,6 +66,10 @@ router.post('/', validateListing, wrapAsync(async (req, res, next) => {
 router.get('/:id/edit', wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash('error', 'Listing not found');
+        return res.redirect('/listings');
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
