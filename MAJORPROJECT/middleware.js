e9400d4d3dcfd3a96610e07d7ae31df9244af290
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-28 18:02:09 +0530

    added new files

--- MAJORPROJECT/middleware.js
+++ MAJORPROJECT/middleware.js
@@ -1,3 +1,9 @@
+const Listing = require('./models/listing');
+const ExpressError = require('./utils/ExpressError.js');
+const { listingSchema , reviewSchema} = require('./schemas.js');
+
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl; // Store the original URL where we wanted to go before our login page interrupted us aand asked us to login first and so that we can redirect the user to that page after they login
@@ -14,3 +20,40 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     }
     next();
 }
+
+// Middleware to check if the user is an owner
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing.owner._id.equals(res.locals.currUser._id)) {
+         req.flash('error', 'You do not have permission to edit this listing');
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+// Middleware to validate the listing data
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+
+};
+
+//function to validate reviews
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+
+};
