58fa197192ead00ffa257e59617e4720dfec9cc9
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-28 19:08:17 +0530

    added new files

--- MAJORPROJECT/middleware.js
+++ MAJORPROJECT/middleware.js
@@ -1,4 +1,5 @@
 const Listing = require('./models/listing');
+const Review= require('./models/review');
 const ExpressError = require('./utils/ExpressError.js');
 const { listingSchema , reviewSchema} = require('./schema.js');
 
@@ -27,6 +28,17 @@ module.exports.isOwner = async (req, res, next) => {
     let listing = await Listing.findById(id);
     if (!listing.owner._id.equals(res.locals.currUser._id)) {
          req.flash('error', 'You do not have permission to edit this listing');
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+// Middleware to check if the user is an author of the review
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id , reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if (!review.author.equals(res.locals.currUser._id)) {
+         req.flash('error', 'ACTION DENIED! You arent the author of this review');
         return res.redirect(`/listings/${id}`);
     }
     next();
