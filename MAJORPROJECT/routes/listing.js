4284f9a1d4c43a04d1d6723e39a4e5fb1a5e296d
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-28 19:08:59 +0530

    added new files

--- MAJORPROJECT/routes/listing.js
+++ MAJORPROJECT/routes/listing.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../utils/wrapAsync.js');
 const Listing = require('../models/listing');
-const { isLoggedIn , isOwner, validateListing} = require('../middleware.js');
+const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
 
 
 
@@ -20,7 +20,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 //show route
 router.get('/:id', wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate('reviews').populate('owner');
+    const listing = await Listing.findById(id).populate({ path: 'reviews', populate: { path: 'author' } }).populate('owner');
     if (!listing) {
         req.flash('error', 'Listing not found');
         return res.redirect('/listings');
--- MAJORPROJECT/routes/review.js
+++ MAJORPROJECT/routes/review.js
@@ -3,13 +3,14 @@ const router = express.Router({ mergeParams: true }); // mergeParams allows us t
 const wrapAsync = require('../utils/wrapAsync.js');
 const Review = require('../models/review.js');
 const Listing = require('../models/listing.js');
-const { validateReview } = require('../middleware.js');
+const { validateReview , isLoggedIn , isReviewAuthor} = require('../middleware.js');
 
 
 //Post route for reviews
-router.post('/', validateReview, wrapAsync(async (req, res) => {
+router.post('/',isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id; // Set the author to the current user
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
@@ -21,7 +22,7 @@ router.post('/', validateReview, wrapAsync(async (req, res) => {
 }));
 
 //Delete route for reviews
-router.delete('/:reviewId', wrapAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
