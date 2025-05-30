12061ab2472e0702976168837eeaf120e62780d6
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-05-30 12:15:42 +0530

    added new files

--- MAJORPROJECT/routes/user.js
+++ MAJORPROJECT/routes/user.js
@@ -6,18 +6,15 @@ const passport = require('passport');
 const { saveRedirectUrl } = require('../middleware.js');
 const userController = require('../controllers/user.js');
 
+router
+    .route("/signup")
+    .get(userController.renderSignupForm)  //Get route for signup form
+    .post(wrapAsync(userController.signup)); //Post route for signup form
 
-//Get route for signup form
-router.get('/signup', userController.renderSignupForm);
-
-//Post route for signup form
-router.post('/signup', wrapAsync(userController.signup));
-
-//Get route for login form
-router.get('/login', userController.renderLoginForm);
-
-//Post route for login form
-router.post('/login',saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
+router
+    .route("/login")
+    .get(userController.renderLoginForm) // Get route for login form
+    .post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
 
 //Get route for logout
 router.get('/logout', userController.logout);
