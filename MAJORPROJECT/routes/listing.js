a8a4748ef0550843185df08e646e85280f8aa5d6
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-06-01 14:51:06 +0530

    added new files

--- MAJORPROJECT/routes/listing.js
+++ MAJORPROJECT/routes/listing.js
@@ -4,13 +4,16 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const Listing = require('../models/listing');
 const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
 const listingController = require('../controllers/listing.js');
+const multer = require('multer'); // For file uploads which parses multipart/form-data
+const {storage} = require('../cloudConfig.js'); // Import Cloudinary storage configuration
+const upload = multer({ storage }); // Directory where uploaded files will be stored
 
 
 router
     .route("/")
     .get(wrapAsync(listingController.index))  //index route
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)); //create route
-
+    .post(isLoggedIn, validateListing, upload.single('listing[image]'), wrapAsync(listingController.createListing)); //create route
+    
     //new route
 router.get('/new', isLoggedIn, listingController.renderNewForm);
 
