f9654c8e50e57d6443d124c82b4d277001c72b8c
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-07-03 12:20:06 +0530

    added new files

---
+++ REACT/amazon-cards-react/src/App.jsx
@@ -0,0 +1,16 @@
+import ProductTab from "./ProductTab.jsx"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>BlockBuster Deals | Shop now</h2>
+    <ProductTab/>
+     
+    </>
+  )
+}
+
+export default App
